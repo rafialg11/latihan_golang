bb81e5fcf4db905f8b002293e9bc593e3cf40ce5
Rafi Alghifari <raffialg11@gmail.com>
2023-09-20 14:28:42 +0700

    init project

---
+++ latihan1/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+)
+
+func luasTrapesium(a, b, t float64) float64 {
+	return (a + b) * t / 2
+}
+
+func isPrime(n int) bool {
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func isMultipleOfSeven(n int) bool {
+	if n%7 == 0 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	fmt.Println("Latihan")
+
+	fmt.Println("1. Luas Trapesium")
+	fmt.Println("2. Bilangan Prima")
+	fmt.Println("3. Kelipatan 7")
+	fmt.Println("Pilih fungsi yang akan dijalankan: ")
+
+	var pilihan int
+	fmt.Scan(&pilihan)
+
+	switch pilihan {
+	case 1:
+		var a, b, t float64
+		fmt.Scan(&a, &b, &t)
+		fmt.Println(luasTrapesium(a, b, t))
+	case 2:
+		var angka int
+		fmt.Print("Masukkan angka: ")
+		fmt.Scan(&angka)
+		fmt.Println(isPrime(angka))
+	case 3:
+		var angka int
+		fmt.Print("Masukkan angka: ")
+		fmt.Scan(&angka)
+		fmt.Println(isMultipleOfSeven(angka))
+	default:
+		fmt.Println("Pilihan tidak tersedia")
+	}
+}
